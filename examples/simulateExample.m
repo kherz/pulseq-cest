a0a2a6e7057558e449d7bba51381d40bb001540c
mzaiss <moritz.zaiss@uk-erlangen.de>
2022-11-14 12:48:47 +0100

    removed which again

--- examples/simulateExample.m
+++ examples/simulateExample.m
@@ -2,11 +2,20 @@
 %
 % kai.herz@tuebingen.mpg.de
 
+%% filenames for .seq-file and simulation parameters
+% get correct path
+script_fp = []; 
+if strcmp(mfilename, 'LiveEditorEvaluationHelperESectionEval')
+    script_fp = fileparts(matlab.desktop.editor.getActiveFilename);
+else
+    script_fp = fileparts(which(mfilename));
+end
+
 %% filenames
 % .seq-file: This file contains the saturation period
-seq_fn   = which('APTw_3T_example.seq'); 
+seq_fn   = fullfile(script_fp, 'APTw_3T_example.seq'); 
 % .yaml-file: This file contains all the simulation settings
-param_fn = which('GM_3T_example_bmsim.yaml'); 
+param_fn = fullfile(script_fp, 'GM_3T_example_bmsim.yaml'); 
 
 %% run simulation
 M_z = simulate_pulseqcest(seq_fn, param_fn);
